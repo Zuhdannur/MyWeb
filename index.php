950067e09aaf1da7b12908fd7708b8d371066e77
Zuhdannur <pczuhdan33@gmail.com>
2018-01-06 17:59:43 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+
+	<title>Document</title>
+	<link rel="stylesheet" href="asset/css/bootstrap.min.css">
+	<link rel="stylesheet" href="asset/css/mdb.min.css">
+	<link rel="stylesheet" href="asset/css/bootstrap.css">
+	<style media="screen">
+		body{
+			margin: 0;
+		}
+		#bac{
+			height: 100vh;
+		}
+		.posisi{
+			position: absolute;
+			top: 45%;
+		}
+		.posisi2{
+			position: absolute;
+			left: 60%;
+		}
+	</style>
+</head>
+<body>
+<div class="fixed-top" style="margin-top:4vh;">
+	<div class="container">
+		<nav class="navbar navbar-expand-lg navbar-light bg-dark" style="opacity:0.8;">
+			<a href="#" class="navbar-brand">
+					<div class="text-white">
+						Tutorials
+					</div></a>
+			<!-- <div class="navbar-nav">
+				<a class="nav-item nav-link" href="#">Home<span class="sr-only"></a>
+			</div> -->
+				<ul class="navbar-nav">
+						<li class="nav-item	">
+							<a class="nav-link" href="#">Home</a>
+						</li>
+				</ul>
+		</nav>
+	</div>
+
+</div>
+<img class="img-fluid" src="asset/img/image1.jpg" alt="" width="100%" id="bac">
+<div class="posisi">
+	<div class="container-fluid">
+		<h1 class="display-5 text-white">TUTORIALS</h1>
+		<p class="font-weight-normal text-white">WebSite Tutorials</p>
+	</div>
+</div>
+<div class="jumbotron">
+	<div class="container" style="margin-top:10vh;">
+		<div class="row">
+				<div class="col-sm-4">
+					<h1 class="display-4 text-center">Android</h1>
+
+					<p class="lead text-center">The world’s most popular mobile OS</p>
+					<br>
+					<center>
+<img src="asset/img/android_logo.png" alt="" width="100vh">
+</center>
+				</div>
+				<div class="col-sm-4">
+						<h1 class="display-4 text-center">C#</h1>
+						<p class="lead text-center">develop software for Linux, Android, and iOS systems as well.</p>
+						<center>
+						<img src="asset/img/csharp.jpg" alt="" width="100vh">
+					</center>
+				</div>
+				<div class="col-sm-4">
+					<h1 class="display-4 text-center">PHP</h1>
+					<p class="lead text-center">The world’s most popular programming Language</p>
+					<center>
+					<img src="asset/img/php_logo.png" alt="" width="100vh">
+				</center>
+				</div>
+		</div>
+		<div class="container">
+			<div class="text-center">
+					<a class="btn btn-primary" href="sign.php" style="margin-top:18	vh;margin-bottom:15vh;">Belajar Sekarang</a>
+			</div>
+
+
+		</div>
+	</div>
+
+
+</div>
+<!-- Ini Untuk content -->
+
+
+
+</body>
+</html>
